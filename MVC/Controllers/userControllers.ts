bd9f9f1e7a942a3aa9caf09f8a2b5e8ea00b359e
LangaLee <langa4749@gmail.com>
2024-07-31 20:28:20 +0000

    removes console.log in users model

--- MVC/Controllers/userControllers.ts
+++ MVC/Controllers/userControllers.ts
@@ -2,7 +2,6 @@ import findUsers from "../Models/userModels";
 import express from "express";
 async function getUsers(req: express.Request, res: express.Response) {
   const users = await findUsers();
-  console.log(users);
   res.status(200).send({ users });
 }
 
